c4f1d27c7924af03ac710e50453f7da2d947ed0b
ruicc <ruicc.rail@gmail.com>
2012-06-09 16:54:41 +0900

    コード整形

--- lifegame.hs
+++ lifegame.hs
@@ -5,7 +5,6 @@ import qualified Data.Array.Accelerate.CUDA as CUDA (run)
 import System.Random (getStdGen, randomRs)
 import Control.Concurrent (threadDelay)
 import Control.Applicative
-import Data.Time (getCurrentTime)
 
 type BoardSize = (Int, Int)
 type Board = Array DIM2 Int
@@ -15,20 +14,14 @@ initLifeGame (x,y) = do
     gen <- getStdGen
     return $ Acc.fromList (Z:.x:.y) $ Prelude.take (x*y) $ randomRs (0,1) gen
 
---stencil2D :: (Elt a, IsScalar a, Integral (Exp a)) => Stencil3x3 a -> Exp a
-stencil2D :: Stencil3x3 Int -> Exp Int
-stencil2D ( (a, b, c), (d, e, f), (g, h, i) ) 
-    = (cnt ==* 2) ? (e, (cnt ==* 3) ? (1, 0))
---    = cnt / 9
---    | cnt ==* 2  = e
---    | cnt ==* 3  = 1
---    | otherwise  = 0
-    where 
-        cnt = a+b+c+d+f+g+h+i
-
-
 step :: Board -> Acc Board
 step arr = Acc.stencil stencil2D (Constant 0) . Acc.use $ arr
+    where
+        --stencil2D :: (Elt a, IsScalar a, Integral (Exp a)) => Stencil3x3 a -> Exp a
+        stencil2D :: Stencil3x3 Int -> Exp Int
+        stencil2D ((a,b,c), (d,e,f), (g,h,i)) = (cnt ==* 2) ? (e, (cnt ==* 3) ? (1, 0))
+            where 
+                cnt = a+b+c+d+f+g+h+i
 
 repeatStep :: BoardSize -> Board -> IO ()
 repeatStep siz board = do
@@ -36,28 +29,22 @@ repeatStep siz board = do
     threadDelay 120000
     clearDisp
     printRect siz board'
---    printOnlyDate
     repeatStep siz board'
     where
-        printOnlyDate = print =<< getCurrentTime
         y = Prelude.snd siz
-        printRect _ board = printRect' $ toList board
-        printRect' board 
-            | length board < y = return ()
+        printRect _ brd = printRect' $ toList brd
+        printRect' brd 
+            | length brd < y = return ()
             | otherwise = do
-                mapM_ (putStr.show) $ Prelude.take y board
+                mapM_ (putStr.show) $ Prelude.take y brd
                 putChar '\n'
-                return ()
-                printRect' $ Prelude.drop y board
-clearDisp = putStr "\x1b[2J" >> putStr "\x1b[1;1H"
+                printRect' $ Prelude.drop y brd
+        clearDisp = putStr "\x1b[2J" >> putStr "\x1b[1;1H"
 
+main :: IO ()
 main = do
-    putStr "x? >"
     x <- read <$> getLine
-    putStr "y? >"
     y <- read <$> getLine
-    putStr "initalizing..."
-    clearDisp
     let siz = (x,y)
     board <- initLifeGame siz
     repeatStep siz board
